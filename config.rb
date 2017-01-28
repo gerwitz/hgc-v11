e6d44b379eda419dda937a1fcd5760c3e8e3e26e
Hans Gerwitz <robots@theartificial.nl>
2017-01-28 22:36:08 +0100

    config cleanup

--- config.rb
+++ config.rb
@@ -1,44 +1,3 @@
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-#page "/*", layout: :page
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# activate :livereload
-
 # Methods defined in the helpers block are available in templates
 helpers do
     def nav_link(link_text, url, options = {})
@@ -56,10 +15,10 @@ set :images_dir, 'images'
 
 # set :relative_links, true
 
-set :markdown_engine, :redcarpet
-set :markdown, fenced_code_blocks: true, smartypants: true
-
-activate :automatic_image_sizes
+set :markdown_engine, :kramdown
+set :markdown, fenced_code_blocks: true,
+               autolink: true,
+               smartypants: true
 
 # assume HTML so we don't have to name source files with destination extensions
 ::Rack::Mime::MIME_TYPES[''] = 'text/html'
